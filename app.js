1d46c8c07eff80fe473c54971324aecef4d3a379
Mohamed Alaa <hi@xaymup.me>
2024-09-07 14:02:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -295,7 +295,7 @@ async function createPlaylist(userId, name) {
         },
         body: JSON.stringify({
             name: name,
-            description: "Generated using xaymup.me/djset-generator",
+            description: "Generated using https://xaymup.me/djset-generator",
             public: false
         })
     });
